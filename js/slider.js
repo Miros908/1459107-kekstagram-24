43f1c357b9c7385549bbb9e26cd15e4163c77fe8
ваше имя <84718361+Miros908@users.noreply.github.com>
2021-11-19 01:14:41 +0200

    ,,

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,6 @@
 
 
-export const getSlider=(slidercontainer,stepsize,min,max)=>{
+export const getSlider=(slidercontainer,stepsize,min,max,meaning)=>{
   noUiSlider.create(slidercontainer, {
     start: [0],
 
@@ -10,6 +10,10 @@ export const getSlider=(slidercontainer,stepsize,min,max)=>{
       'min': min,
       'max': max,
     },
+    format: wNumb({
+
+      suffix: meaning,
+    }),
 
 
   });
